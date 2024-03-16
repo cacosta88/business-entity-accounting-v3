bfd8750720d6df1653f4e550c62d05fcb3661a2d
pipoquinha <caio.costa@me.com>
2024-03-16 19:12:46 -0300

    Add new menu link to Header component

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, CurrencyDollarIcon, ShoppingBagIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -14,8 +14,14 @@ type HeaderMenuLink = {
 
 export const menuLinks: HeaderMenuLink[] = [
   {
-    label: "Home",
+    label: "Dashboard",
     href: "/",
+    icon: <CurrencyDollarIcon className="h-4 w-4" />,
+  },
+  {
+    label: "Invoice Portal",
+    href: "/invoice-portal",
+    icon: <ShoppingBagIcon className="h-4 w-4" />,
   },
   {
     label: "Debug Contracts",
